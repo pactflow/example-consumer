d01d910c49d68adeb99815d747b51e5b455f25d3
Matt Fellows <matt.fellows@onegeek.com.au>
2021-11-23 23:36:07 +1100

    chore: remove price from Product class to avoid confusion

--- src/product.js
+++ src/product.js
@@ -1,8 +1,7 @@
 export class Product {
-  constructor({id, name, type, price}) {
+  constructor({id, name, type}) {
     this.id = id
     this.name = name
     this.type = type
-    this.price = price
   }
 }
