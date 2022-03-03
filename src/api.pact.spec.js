30698cb32f2cf7c7eeecad5fef34aa7b750a3bf0
teixeiraf <fernando.teixeira@verivox.com>
2022-03-03 15:07:03 +0100

    another change in the contract

--- src/api.pact.spec.js
+++ src/api.pact.spec.js
@@ -33,7 +33,7 @@ describe('API Pact test', () => {
           },
         },
         willRespondWith: {
-          status: 200,
+          status: 400,
           headers: {
             'Content-Type': regex({generate: 'application/json; charset=utf-8', matcher: 'application/json;?.*'}),
           },
