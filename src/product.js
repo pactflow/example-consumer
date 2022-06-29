507df2f84029cbb93761c6b6696f513d6ff043b9
hazem.borham <hazem@tendosystems.com>
2022-06-29 14:31:29 -0700

    add tapable

--- src/product.js
+++ src/product.js
@@ -1,7 +1,8 @@
 export class Product {
-  constructor({id, name, type}) {
+  constructor({id, name, type, tapable}) {
     this.id = id
     this.name = name
     this.type = type
+    this.tapable = tapable
   }
 }
