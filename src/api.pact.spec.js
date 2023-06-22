4288bcb7c08754673b15e38ca1d2857c62a6aac2
Mike Swiss <mikeswiss@discovery.com>
2023-06-21 20:57:33 -0400

    add red field

--- src/api.pact.spec.js
+++ src/api.pact.spec.js
@@ -19,7 +19,8 @@ describe('API Pact test', () => {
       const expectedProduct = {
         id: '10',
         type: 'CREDIT_CARD',
-        name: '28 Degrees'
+        name: '28 Degrees',
+        color: "red"
       };
 
       // Uncomment to see this fail
