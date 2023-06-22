01b60bb043be0712a9f5646c662a9c4d9c40e03b
Mike Swiss <mikeswiss@discovery.com>
2023-06-22 09:25:48 -0400

    remove red field

--- src/api.pact.spec.js
+++ src/api.pact.spec.js
@@ -19,8 +19,7 @@ describe('API Pact test', () => {
       const expectedProduct = {
         id: '10',
         type: 'CREDIT_CARD',
-        name: '28 Degrees',
-        color: "red"
+        name: '28 Degrees'
       };
 
       // Uncomment to see this fail
