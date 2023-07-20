3e9e03afaca3f2ece97f155698fa6ab80b420333
metalix2 <matthew.robertson@skyscanner.net>
2023-07-20 14:12:48 +0100

    update test

--- src/api.pact.spec.js
+++ src/api.pact.spec.js
@@ -34,7 +34,8 @@ describe('API Pact test', () => {
           method: 'GET',
           path: '/product/10',
           headers: {
-            'X-Authorization': like('Bearer 2019-01-14T11:34:18.045Z')
+            'X-Authorization': like('Bearer 2019-01-14T11:34:18.045Z'),
+            Authorization: like('Bearer 2019-01-14T11:34:18.045Z')
           }
         })
         .willRespondWith({
@@ -65,7 +66,8 @@ describe('API Pact test', () => {
           method: 'GET',
           path: '/product/11',
           headers: {
-            'X-Authorization': like('Bearer 2019-01-14T11:34:18.045Z')
+            'X-Authorization': like('Bearer 2019-01-14T11:34:18.045Z'),
+            Authorization: like('Bearer 2019-01-14T11:34:18.045Z')
           }
         })
         .willRespondWith({
@@ -98,7 +100,8 @@ describe('API Pact test', () => {
           method: 'GET',
           path: '/products',
           headers: {
-            'X-Authorization': like('Bearer 2019-01-14T11:34:18.045Z')
+            'X-Authorization': like('Bearer 2019-01-14T11:34:18.045Z'),
+            Authorization: like('Bearer 2019-01-14T11:34:18.045Z')
           }
         })
         .willRespondWith({
@@ -136,7 +139,8 @@ describe('API Pact test', () => {
           method: 'GET',
           path: '/user/1',
           headers: {
-            'X-Authorization': like('Bearer 2019-01-14T11:34:18.045Z')
+            'X-Authorization': like('Bearer 2019-01-14T11:34:18.045Z'),
+            Authorization: like('Bearer 2019-01-14T11:34:18.045Z')
           }
         })
         .willRespondWith({
