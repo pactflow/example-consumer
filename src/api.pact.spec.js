58b3726ff058a30ae96bcb9354558909a67a92e9
Alexander <sasha133044@gmail.com>
2023-10-22 21:19:12 +0300

    SOB-000 Revert test change

--- src/api.pact.spec.js
+++ src/api.pact.spec.js
@@ -86,8 +86,7 @@ describe('API Pact test', () => {
       const expectedProduct = {
         id: '10',
         type: 'CREDIT_CARD',
-        name: '28 Degrees',
-        color: "red"
+        name: '28 Degrees'
       };
 
       mockProvider
