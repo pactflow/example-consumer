9c4a509bdb7c838ef9dbff05db26751973763aad
Antal Tony Tettinger <antal.tettinger@gmail.com>
2023-12-07 20:14:01 +0100

    feat: add color

--- src/api.pact.spec.js
+++ src/api.pact.spec.js
@@ -20,6 +20,7 @@ describe("API Pact test", () => {
         id: "10",
         type: "CREDIT_CARD",
         name: "28 Degrees",
+        color: "red",
       }
 
       // Uncomment to see this fail
