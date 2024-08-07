ec3345f4c4a679ad04a9176dabcd67488f61befe
prateek0411999 <prateekshrmprateek@gmail.com>
2024-08-07 14:15:32 +0530

    changed pact

--- src/product.js
+++ src/product.js
@@ -1,7 +1,8 @@
 export class Product {
-  constructor({id, name, type}) {
+  constructor({id, name, type, date}) {
     this.id = id
     this.name = name
     this.type = type
+    this.date = date
   }
 }
