e04bbfb7ae164788161a9185275d3fb45f9dec1f
Matt Fellows <53900+mefellows@users.noreply.github.com>
2024-09-13 18:14:27 +1000

    chore: updates to product object

--- src/product.js
+++ src/product.js
@@ -1,8 +1,7 @@
 export class Product {
-  constructor({id, name, type, foobar}) {
+  constructor({id, name, type}) {
     this.id = id
     this.name = name
     this.type = type
-    this.foobar = foobar
   }
 }
