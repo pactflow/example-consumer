66ae4955aa8b7c99384c0b1c0bc77b9de2884a94
Kinkini Gamage <kinkini.gamage@wrkr.com.au>
2025-07-12 08:37:58 +1000

    adding color

--- src/api.pact.spec.js
+++ src/api.pact.spec.js
@@ -20,7 +20,7 @@ describe('API Pact test', () => {
         id: '10',
         type: 'CREDIT_CARD',
         name: '28 Degrees',
-        color: "red"
+        color: 'red'
       };
 
       // Uncomment to see this fail
