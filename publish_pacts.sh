74f6f8c2ce9b34b6dd9c746b863f12faf37b243b
Tuan Pham <tuan.pham@smartbear.com>
2025-09-22 18:19:51 +1000

    chore: using pact-js-cli to publish contract

--- publish_pacts.sh
+++
@@ -1,7 +0,0 @@
-export $(cat .env | xargs)
-
-GIT_COMMIT="$(git rev-parse HEAD)"
-GIT_BRANCH="$(git rev-parse --abbrev-ref HEAD)"
-
-PACT_CLI="docker run --rm -v ${PWD}:${PWD} -e PACT_BROKER_BASE_URL -e PACT_BROKER_TOKEN pactfoundation/pact-cli"
-${PACT_CLI} publish ${PWD}/pacts --consumer-app-version ${GIT_COMMIT} --branch ${GIT_BRANCH}
